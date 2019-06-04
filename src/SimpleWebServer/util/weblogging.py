11e2506b5a9aea6550e5d98e3198ea88aa2cc71c
Administrator <Administrator@TC503-1>
2019-06-04 12:00:29 +0800

    web

---
+++ src/SimpleWebServer/util/weblogging.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+
+
+
+def weblog(weblogstr,ip,path):
+    logging.basicConfig(filename=weblogstr,format='%(message)s - %(asctime)s',level="DEBUG")
+    msg = ip + " "+path
+    logging.info(msg)
+    
+# weblog("10.10.10.10", "/check")
+
+  
+# weblog("10.35.10.10","/check")
+    
--- src/SimpleWebServer/web/controller/web.py
+++ src/SimpleWebServer/web/controller/web.py
@@ -4,6 +4,7 @@ from SimpleWebServer.web.myjinja import html
 from urllib.parse import parse_qs
 from SimpleWebServer.util.txtutil import checkUserAndPwd
 from SimpleWebServer.web.model.model import Model
+import logging
 
 async def check(info):
 #     print(parse_qs(info.query_string))
@@ -23,18 +24,39 @@ async def check(info):
     return web.Response(body=html("msg.html",**locals()),content_type="text/html")
     
 
-
+def weblog(weblogstr,ip,path):
+    logging.basicConfig(filename=weblogstr,format='%(message)s - %(asctime)s',level="DEBUG")
+    msg = ip + " "+path
+    logging.info(msg)
+    
 async def index(request):
     data=""
+    
+    ip = request._transport_peername[0]
+    path = request.message.url
+    logfile =os.path.abspath("../../logs/web.log") 
+    print(logfile)
+    await weblog(logfile,ip, path)
+
     with open("../views/login.html",encoding="utf-8") as f:
         data = f.read()
     data1 = web.Response(body=data,content_type="text/html")
     return data1
-
+import os
 async def init(loop):
     app = web.Application()
+#     infos = [r for r in app.router.resources()]
+#     print(type(infos))
+#     if infos:
+#         print(infos[0].split("\n\r")) 
+#     
+#     logfile = "../../logs/web.log"
+#     print(os.path.abspath(logfile))
+#     ip = ""
+#     path = ""
+#     weblog(logfile,ip, path)
     app.router.add_route('GET', '/', index)
-    app.router.add_route('GET', '/check', check)
+    app.router.add_route('GET', '/check', check)    
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner=runner,port=9000)
